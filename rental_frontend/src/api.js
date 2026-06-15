1d1699f9739476f76ebeb457225585734f13c401
Zwivhuya Mathada <221584226@mycput.ac.za>
2026-06-15 15:51:18 +0200

    Hosting

--- rental_frontend/src/api.js
+++ rental_frontend/src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://127.0.0.1:8000" });
-
+const api = axios.create({ baseURL: "https://rental-management-system-hh5a.onrender.com" });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
